4c6840f480d6897a76cfdd9d91da45bb9b94b95b
Leo Ribas <leonas.ribas@gmail.com>
2021-05-06 22:25:12 -0300

    ✨ Adiciona model User

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 6 }
+  validates :name, presence: true
+  validates :phone, presence: true
+end
